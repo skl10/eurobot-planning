99246d227ae08d73d44c46b4b56402d4e55e90da
Susie <susielee95@gmail.com>
2018-03-26 21:52:49 +0100

    Naming

--- ball_task.py
+++ ball_task.py
@@ -1,21 +1,21 @@
 from planner import get_time
 
 
-def navigate_to(location):
+def navigateTo(location):
 #	go to location
 #	(this finishes executing when robot has arrived at goal location)
 
-def open_latch():
+def openLatch():
 #	open the latch in front of us -- make adjustments to pose as necessary
 
-def collect_balls():
+def collectBalls():
 #	get balls from recuperator and categorise them
 
-def deposit_balls():
+def depositBalls():
 #   navigate to appropriate tower
 #   deposit balls
 
-our_tower = True
+ourTower = True
 # our_tower: boolean value that keeps track of whether
 # we’re going to our tower or not (makes it easier to generalise
 # code while navigating, and enables us to know which
@@ -26,48 +26,48 @@ our_tower = True
 # also add something that publishes when task has been completed
 #
 
-max_time = 1000 # placeholder: how long can we do this task for?
+maxTime = 1000 # placeholder: how long can we do this task for?
 
-start_time = get_time()
+startTime = getTime()
 
-while (get_time() - start_time) < max_time:
+while (getTime() - startTime) < maxTime:
 
 # THIS DEPENDS ON HOW BALL COLLECTION, SORTING, DEPOSITING ENDS UP HAPPENING
 
-	navigate_to(first_recup_location)
+	navigateTo(firstRecupLocation)
 	# I will assume that navigate_to stops executing only once we arrive
 	# at our chosen location
 
-	open_latch()
+	openLatch()
 
-	collect_balls()
+	collectBalls()
 
 	# if we have capacity for more balls, go to more recuperators? or drop off
 	# on the way? depends on locations
 
-	navigate_to(second_recup_location)
+	navigateTo(secondRecupLocation)
 
-	open_latch()
+	openLatch()
 
-	collect_balls()
+	collectBalls()
 
 
-	if(dist_to_our_tower <= dist_to_their_tower):
+	if(distToOurTower <= distToTheirTower):
 	# [note: any reason to choose one over the other if distance is equal?]
-		our_tower = True # keeps track of which tower we're at
-		navigate_to(our_tower_location)
-		deposit_balls(our_tower)
+		ourTower = True # keeps track of which tower we're at
+		navigateTo(ourTowerLocation)
+		depositBalls(ourTower)
 
-		navigate_to(their_tower_location)
-		our_tower = False
-		deposit_balls(our_tower)
+		navigateTo(theirTowerLocation)
+		ourTower = False
+		depositBalls(ourTower)
 
-	else if (dist_to_their_tower < dist_to_our_tower):
-		our_tower = False
-		navigate_to(their_tower_location)
-		deposit_balls(our_tower)
+	else if (distToTheirTower < distToOurTower):
+		ourTower = False
+		navigateTo(theirTowerLocation)
+		depositBalls(ourTower)
 
-		navigate_to(our_tower_location)
-		our_tower = True
-		deposit_balls(our_tower)
+		navigateTo(ourTowerLocation)
+		ourTower = True
+		depositBalls(ourTower)
 
